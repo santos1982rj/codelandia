1bf28dd83edfba923fa4ff4b3cc7f6df680123ff
santos1982rj <viniciussantosni@outlook.com>
2021-12-27 02:26:36 -0300

    Desafio 16 - Primeiro commit

---
+++ desafio-16/assets/js/main.js
@@ -0,0 +1,12 @@
+// MENU TOGGLE
+
+let show = true;
+
+const menuMobile = document.querySelector('.menu-mobile');
+const menuToggle = menuMobile.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+  document.body.style.overflow = show ? 'hidden' : 'initial';
+  menuMobile.classList.toggle('on', show);
+  show = !show;
+});
