6ec50de377fdcbf03b59aa52130ab4636e5fa6bf
santos1982rj <viniciussantosni@outlook.com>
2022-02-25 10:49:29 -0300

    Desafio 20 - Theme Style and Scroll top

--- desafio-20/js/app.js
+++ desafio-20/js/app.js
@@ -1,6 +1,8 @@
 // REPLACE FEATHER ICONS
 feather.replace();
 
+// THEME Style
+
 // MENU RESPONSIVO
 
 const toggle = document.querySelector('.menu-toggle');
@@ -33,6 +35,19 @@ setTimeout(function () {
   const teste = document.querySelectorAll('.vortex path');
   teste.forEach((item, index) => {
     teste[index].style.animationDelay += index * 0.1 + 's';
-    console.log((teste[index].style.animationDelay += index * 0.1 + 's'));
   });
 }, 500);
+
+// SCROLL top
+
+const scrollTop = document.querySelector('.scroll-top');
+
+scrollTop.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+const toggleTheme = document.querySelector('.toggle-theme');
+
+toggleTheme.addEventListener('click', () => {
+  document.body.classList.toggle('lighttheme');
+});
